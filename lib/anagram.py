ad3dc3d95b7e3bfcd903a94b2ba74a4d95e6ab0b
Priscilla-22 <priscillamw123@gmail.com>
2024-03-03 12:06:41 +0300

    feat: anagram

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,15 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, list):
+        return [
+            letter
+            for letter in list
+            if sorted(letter) == sorted(self.word) and letter != self.word
+        ]
+
+
+words = Anagram("listen")
+print(words.match(["enlists", "google", "inlets", "banana"]))
